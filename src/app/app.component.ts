a889d8e9fa9be150ce77580d57afdaaf6914467c
scottmacaz <scottmacaz@yahoo.com>
2018-06-08 10:35:46 -0700

    2D Array for plate positions created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PlateLayout} from './plate-layout';
+import {PlatePosition} from './plate-position';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
+export class AppComponent implements OnInit {
+
+  plateLayout: PlateLayout;
+
+  ngOnInit(): void {
+    const platePositions = this.makePlatePositions();
+    this.plateLayout = new PlateLayout(platePositions);
+  }
+
+  makePlatePositions() {
+    let id = 0;
+    const platePositions: PlatePosition[][] = [];
+    for (let row = 0; row < 8; row++) {
+      platePositions[row] = [];
+      for (let column = 0; column < 12; column++) {
+        platePositions[row][column] = new PlatePosition(id, row + 1, column + 1, `plate-pos-${row + 1}-${column + 1}`);
+        id++;
+      }
+    }
+    return platePositions;
+  }
 }
--- src/app/plate-svg/plate-svg.component.ts
+++ src/app/plate-svg/plate-svg.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PlateLayout } from '../plate-layout';
 
 @Component({
   selector: 'app-plate-svg',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plate-svg.component.css']
 })
 export class PlateSvgComponent implements OnInit {
+
+  @Input() plateLayout: PlateLayout;
 
   constructor() { }
 
